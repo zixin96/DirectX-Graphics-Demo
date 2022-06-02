6c3b5faed40968b406680458c37f22148ab73d59
Zixin Zhang <zixinzha@seas.upenn.edu>
2022-06-02 17:30:21 -0400

    Exercise 2 complete

---
+++ Chapter 7 Drawing in Direct3D Part II/Ex2/Shaders/color.hlsl
@@ -0,0 +1,85 @@
+//***************************************************************************************
+// color.hlsl by Frank Luna (C) 2015 All Rights Reserved.
+//
+// Transforms and colors geometry.
+//***************************************************************************************
+ 
+cbuffer cbPerObject : register(b0)
+{
+    // we cannot have float4x4 in a cbuffer that gets mapped onto root constants
+	float a; 
+	float b; 
+	float c; 
+	float d; 
+
+	float e; 
+	float f; 
+	float g; 
+	float h; 
+
+    float i; 
+	float j; 
+	float k; 
+	float l; 
+
+	float m; 
+	float n; 
+	float o; 
+	float p; 
+};
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+};
+
+struct VertexIn
+{
+	float3 PosL  : POSITION;
+    float4 Color : COLOR;
+};
+
+struct VertexOut
+{
+	float4 PosH  : SV_POSITION;
+    float4 Color : COLOR;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+	float4x4 gWorld = {
+        a,b,c,d,
+        e,f,g,h,
+        i,j,k,l,
+        m,n,o,p,
+    };
+	// Transform to homogeneous clip space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosH = mul(posW, gViewProj);
+	
+	// Just pass vertex color into the pixel shader.
+    vout.Color = vin.Color;
+    
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    return pin.Color;
+}
+
+
