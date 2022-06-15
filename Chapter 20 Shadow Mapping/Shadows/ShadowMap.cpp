9bba6395a13885c7700dbe3458a7534d749d3c27
Zixin Zhang <zixinzha@seas.upenn.edu>
2022-06-15 15:40:25 -0400

    Update

--- Chapter 20 Shadow Mapping/Shadows/ShadowMap.cpp
+++ Chapter 20 Shadow Mapping/Shadows/ShadowMap.cpp
@@ -3,63 +3,81 @@
 //***************************************************************************************
 
 #include "ShadowMap.h"
- 
+
 ShadowMap::ShadowMap(ID3D12Device* device, UINT width, UINT height)
 {
 	md3dDevice = device;
 
-	mWidth = width;
+	// Note: the resolution of the shadow map affects the quality of our shadows
+	// High resolution => more expensive to render into, more memory, better quality
+	mWidth  = width;
 	mHeight = height;
 
-	mViewport = { 0.0f, 0.0f, (float)width, (float)height, 0.0f, 1.0f };
-	mScissorRect = { 0, 0, (int)width, (int)height };
+	mViewport = {
+		0.0f,          // X position of the left hand side of the viewport
+		0.0f,          // Y position of the top of the viewport
+		(float)width,  // Width of the viewport
+		(float)height, // Height of the viewport
+		0.0f,          // Minimum depth of the viewport
+		1.0f           // Maximum depth of the viewport
+	};
 
+	mScissorRect = {
+		0,          // left
+		0,          // top
+		(int)width, // right
+		(int)height // botoom
+	};
+
+	// creates a depth/stencil texture of the specified dimensions and viewport
 	BuildResource();
 }
 
-UINT ShadowMap::Width()const
+UINT ShadowMap::Width() const
 {
-    return mWidth;
+	return mWidth;
 }
 
-UINT ShadowMap::Height()const
+UINT ShadowMap::Height() const
 {
-    return mHeight;
+	return mHeight;
 }
 
-ID3D12Resource*  ShadowMap::Resource()
+ID3D12Resource* ShadowMap::Resource()
 {
 	return mShadowMap.Get();
 }
 
-CD3DX12_GPU_DESCRIPTOR_HANDLE ShadowMap::Srv()const
+CD3DX12_GPU_DESCRIPTOR_HANDLE ShadowMap::Srv() const
 {
+	// shadow map will be used as shader input
 	return mhGpuSrv;
 }
 
-CD3DX12_CPU_DESCRIPTOR_HANDLE ShadowMap::Dsv()const
+CD3DX12_CPU_DESCRIPTOR_HANDLE ShadowMap::Dsv() const
 {
+	// shadow map will be used as a render target 
 	return mhCpuDsv;
 }
 
-D3D12_VIEWPORT ShadowMap::Viewport()const
+D3D12_VIEWPORT ShadowMap::Viewport() const
 {
 	return mViewport;
 }
 
-D3D12_RECT ShadowMap::ScissorRect()const
+D3D12_RECT ShadowMap::ScissorRect() const
 {
 	return mScissorRect;
 }
 
 void ShadowMap::BuildDescriptors(CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuSrv,
-	                             CD3DX12_GPU_DESCRIPTOR_HANDLE hGpuSrv,
-	                             CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuDsv)
+                                 CD3DX12_GPU_DESCRIPTOR_HANDLE hGpuSrv,
+                                 CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuDsv)
 {
 	// Save references to the descriptors. 
 	mhCpuSrv = hCpuSrv;
 	mhGpuSrv = hGpuSrv;
-    mhCpuDsv = hCpuDsv;
+	mhCpuDsv = hCpuDsv;
 
 	//  Create the descriptors
 	BuildDescriptors();
@@ -67,9 +85,9 @@ void ShadowMap::BuildDescriptors(CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuSrv,
 
 void ShadowMap::OnResize(UINT newWidth, UINT newHeight)
 {
-	if((mWidth != newWidth) || (mHeight != newHeight))
+	if ((mWidth != newWidth) || (mHeight != newHeight))
 	{
-		mWidth = newWidth;
+		mWidth  = newWidth;
 		mHeight = newHeight;
 
 		BuildResource();
@@ -78,26 +96,26 @@ void ShadowMap::OnResize(UINT newWidth, UINT newHeight)
 		BuildDescriptors();
 	}
 }
- 
+
 void ShadowMap::BuildDescriptors()
 {
-    // Create SRV to resource so we can sample the shadow map in a shader program.
+	// Create SRV to resource so we can sample the shadow map in a shader program.
 	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
-	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-    srvDesc.Format = DXGI_FORMAT_R24_UNORM_X8_TYPELESS; 
-	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MostDetailedMip = 0;
-	srvDesc.Texture2D.MipLevels = 1;
-	srvDesc.Texture2D.ResourceMinLODClamp = 0.0f;
-    srvDesc.Texture2D.PlaneSlice = 0;
-    md3dDevice->CreateShaderResourceView(mShadowMap.Get(), &srvDesc, mhCpuSrv);
+	srvDesc.Shader4ComponentMapping         = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.Format                          = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+	srvDesc.ViewDimension                   = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MostDetailedMip       = 0;
+	srvDesc.Texture2D.MipLevels             = 1;
+	srvDesc.Texture2D.ResourceMinLODClamp   = 0.0f;
+	srvDesc.Texture2D.PlaneSlice            = 0;
+	md3dDevice->CreateShaderResourceView(mShadowMap.Get(), &srvDesc, mhCpuSrv);
 
 	// Create DSV to resource so we can render to the shadow map.
-	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc; 
-    dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
-    dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
-    dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-    dsvDesc.Texture2D.MipSlice = 0;
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc;
+	dsvDesc.Flags              = D3D12_DSV_FLAG_NONE;
+	dsvDesc.ViewDimension      = D3D12_DSV_DIMENSION_TEXTURE2D;
+	dsvDesc.Format             = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	dsvDesc.Texture2D.MipSlice = 0;
 	md3dDevice->CreateDepthStencilView(mShadowMap.Get(), &dsvDesc, mhCpuDsv);
 }
 
@@ -105,28 +123,28 @@ void ShadowMap::BuildResource()
 {
 	D3D12_RESOURCE_DESC texDesc;
 	ZeroMemory(&texDesc, sizeof(D3D12_RESOURCE_DESC));
-	texDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-	texDesc.Alignment = 0;
-	texDesc.Width = mWidth;
-	texDesc.Height = mHeight;
-	texDesc.DepthOrArraySize = 1;
-	texDesc.MipLevels = 1;
-	texDesc.Format = mFormat;
-	texDesc.SampleDesc.Count = 1;
+	texDesc.Dimension          = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	texDesc.Alignment          = 0;
+	texDesc.Width              = mWidth;
+	texDesc.Height             = mHeight;
+	texDesc.DepthOrArraySize   = 1;
+	texDesc.MipLevels          = 1;
+	texDesc.Format             = mFormat;
+	texDesc.SampleDesc.Count   = 1;
 	texDesc.SampleDesc.Quality = 0;
-	texDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
-	texDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+	texDesc.Layout             = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	texDesc.Flags              = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
 
-    D3D12_CLEAR_VALUE optClear;
-    optClear.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-    optClear.DepthStencil.Depth = 1.0f;
-    optClear.DepthStencil.Stencil = 0;
+	D3D12_CLEAR_VALUE optClear;
+	optClear.Format               = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	optClear.DepthStencil.Depth   = 1.0f;
+	optClear.DepthStencil.Stencil = 0;
 
 	ThrowIfFailed(md3dDevice->CreateCommittedResource(
-		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
-		D3D12_HEAP_FLAG_NONE,
-		&texDesc,
-        D3D12_RESOURCE_STATE_GENERIC_READ,
-		&optClear,
-		IID_PPV_ARGS(&mShadowMap)));
+		              &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		              D3D12_HEAP_FLAG_NONE,
+		              &texDesc,
+		              D3D12_RESOURCE_STATE_GENERIC_READ,
+		              &optClear,
+		              IID_PPV_ARGS(&mShadowMap)));
 }
--- Chapter 20 Shadow Mapping/Shadows/ShadowMap.h
+++ Chapter 20 Shadow Mapping/Shadows/ShadowMap.h
@@ -6,29 +6,41 @@
 
 #include "../../Common/d3dUtil.h"
 
+/**
+ * \brief A utility class that helps us store the scene depth from the perspective of the light source.
+ * It encapsulates a depth/stencil buffer, necessary views, and viewport.
+ * A depth/stencil buffer used for shadow mapping is called a shadow map. 
+ */
 class ShadowMap
 {
 public:
 	ShadowMap(ID3D12Device* device,
-		UINT width, UINT height);
-		
-	ShadowMap(const ShadowMap& rhs)=delete;
-	ShadowMap& operator=(const ShadowMap& rhs)=delete;
-	~ShadowMap()=default;
+	          UINT          width,
+	          UINT          height);
 
-    UINT Width()const;
-    UINT Height()const;
+	ShadowMap(const ShadowMap& rhs)            = delete;
+	ShadowMap& operator=(const ShadowMap& rhs) = delete;
+	~ShadowMap()                               = default;
+
+	UINT Width() const;
+	UINT Height() const;
+
 	ID3D12Resource* Resource();
-	CD3DX12_GPU_DESCRIPTOR_HANDLE Srv()const;
-	CD3DX12_CPU_DESCRIPTOR_HANDLE Dsv()const;
 
-	D3D12_VIEWPORT Viewport()const;
-	D3D12_RECT ScissorRect()const;
+	// The shadow mapping algorithm requires 2 render passes. In the first one,
+	// we render the scene depth from the viewpoint of the light into the shadow map (DSV).
+	// In the second one, we render the scene as normal to the back buffer from our "player" camera,
+	// but use the shadow map as a shader input (SRV) to implement the shadow mapping algorithm.
+	// Thus, the shadow map will be viewed as both depth/stencil (shadow pass) and shader resource (rendering pass)
+	CD3DX12_GPU_DESCRIPTOR_HANDLE Srv() const;
+	CD3DX12_CPU_DESCRIPTOR_HANDLE Dsv() const;
 
-	void BuildDescriptors(
-		CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuSrv,
-		CD3DX12_GPU_DESCRIPTOR_HANDLE hGpuSrv,
-		CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuDsv);
+	D3D12_VIEWPORT Viewport() const;
+	D3D12_RECT     ScissorRect() const;
+
+	void BuildDescriptors(CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuSrv,
+	                      CD3DX12_GPU_DESCRIPTOR_HANDLE hGpuSrv,
+	                      CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuDsv);
 
 	void OnResize(UINT newWidth, UINT newHeight);
 
@@ -37,14 +49,13 @@ private:
 	void BuildResource();
 
 private:
-
 	ID3D12Device* md3dDevice = nullptr;
 
 	D3D12_VIEWPORT mViewport;
-	D3D12_RECT mScissorRect;
+	D3D12_RECT     mScissorRect;
 
-	UINT mWidth = 0;
-	UINT mHeight = 0;
+	UINT        mWidth  = 0;
+	UINT        mHeight = 0;
 	DXGI_FORMAT mFormat = DXGI_FORMAT_R24G8_TYPELESS;
 
 	CD3DX12_CPU_DESCRIPTOR_HANDLE mhCpuSrv;
@@ -53,5 +64,3 @@ private:
 
 	Microsoft::WRL::ComPtr<ID3D12Resource> mShadowMap = nullptr;
 };
-
- 
